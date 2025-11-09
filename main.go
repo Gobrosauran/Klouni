40bdd6c6c09e37aacf2a0ca7925bbbf4c139275a
FcGohack <kuatsauran3@gmail.com>
2025-11-09 11:17:34 +0500

    rem

--- main.go
+++ main.go
@@ -1,1 +1,40 @@
 package main
+
+import (
+	"fmt"
+	"klouni/usprob"
+)
+
+type User struct {
+	Name   string
+	Rating float64
+}
+
+func (u User) NameUSer(name string) {
+	fmt.Println("Меня зовут:", u.Name)
+}
+
+func (u User) Greeting(rating float64) {
+	if u.Rating-rating < 10.0 {
+		fmt.Println("Мой рейтинг:", u.Rating)
+		fmt.Println(u.Rating)
+		return
+	} else if u.Rating+rating == 10.0 {
+		fmt.Println("Мой рейтинг:", u.Rating)
+		return
+
+	}
+}
+
+func main() {
+	user := User{
+		Name:   "Sauran",
+		Rating: 10.0,
+	}
+	fmt.Println("user:", user)
+	user.NameUSer(user.Name)
+	user.Greeting(user.Rating)
+
+	usprob.UserGo(user.Name)
+
+}
---
+++ usprob/user.go
@@ -0,0 +1,18 @@
+package usprob
+
+import (
+	"fmt"
+)
+
+func UserGo(str string) {
+	user1 := str
+	if user1 != "" {
+		fmt.Println("Err!")
+
+	} else {
+		fmt.Println("Hello,", user1)
+
+	}
+	fmt.Println("")
+
+}
